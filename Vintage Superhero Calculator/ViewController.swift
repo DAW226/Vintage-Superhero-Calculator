89350211fe4c7d7fb6fcbc241ae330ff923860f0
Darin A. Wilson <darin.wilson226@gmail.com>
2016-11-08 11:11:21 -0500

    Initial Commit

---
+++ Vintage Superhero Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vintage Superhero Calculator
+//
+//  Created by Darin Wilson on 11/8/16.
+//  Copyright © 2016 Darin Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
